a233f6e2b060992ccb000cba21caf24d302150bd
nimish3110 <43986201+nimish3110@users.noreply.github.com>
2018-10-09 15:46:58 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,13 @@
+def factorial(n): #function_defination
+ f=1
+ if n<0:
+  print('enter valid no.')
+ elif n==0 or n==1:
+  print(1)
+ else:
+  while n>1:
+   f=f*n
+   n=(n-1)
+  print(f)
+n=int(input())
+factorial(n)

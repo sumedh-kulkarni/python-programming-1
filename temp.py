961405e8afc8165ba47cc605ed041c0c2e073748
nimish3110 <43986201+nimish3110@users.noreply.github.com>
2018-11-27 15:13:18 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,9 +1,9 @@
 #temprtute
 #NIMISH GEDAM
 #M46
-x=int(input("Enter temperature:"))
-y=input("is it in 'celcius' or 'temperatur'")
-if y is 'celsius':
+x=int(input("Enter temperature:"))#INPUT STATEMENT
+y=input("is it in 'celcius' or 'temperatur'")#ASKING WHETHER IT IS IN CELSIUS OR FARENITE
+if y is 'celsius':#CONITIONAL STATEMENT
  f=9*x/5 + 32
  print(f)
 else:

16d0a8eab48902664b65bebfc41c18cd63d056a4
sumedh-kulkarni <44223857+sumedh-kulkarni@users.noreply.github.com>
2018-12-03 18:03:23 +0530

    Create primenumber.py

---
+++ primenumber.py
@@ -0,0 +1,13 @@
+#prime number
+#nimish gedam
+#M46
+x=int(input("enter a no."))
+i=2
+y=i
+while i<=x:
+ i=i+1
+if (x%y)==0:
+ print("x is composite no.")
+else:
+ print("x is a prime no.")
+print("done")
